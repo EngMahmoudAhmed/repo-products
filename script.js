4dc4aa9df89c6dcbbb176a0a94c16a428a096f42
Mahmoud-Ahmed <lya440676@gmail.com>
2023-01-03 09:11:20 +0200

    add Search product

--- script.js
+++ script.js
@@ -1,61 +1,47 @@
-// var productNameInput = document.getElementById('productName');
-// var productPriceInput = document.getElementById('productPrice');
-// var productCategoryInput = document.getElementById('productCategory');
-// var productDescInput = document.getElementById('productDesc');
-// var productsContainer;
+var productNameInput = document.getElementById('productName');
+var productPriceInput = document.getElementById('productPrice');
+var productCategoryInput = document.getElementById('productCategory');
+var productDescInput = document.getElementById('productDesc');
+var productContainer;
 
-// if (localStorage.getItem('products') == null) {
-//     productsContainer = [];
-// }
-// else {
-//     productsContainer = JSON.parse(localStorage.getItem("products"))
-//     displayProduct();
-// }
-
-// function addProduct() {
-//     var products = {
-//         name: productNameInput.value,
-//         price: productPriceInput.value,
-//         category: productCategoryInput.value,
-//         desc: productDescInput.value,
-//     }
-//     // console.log(products)
-//     productsContainer.push(products);
-//     localStorage.setItem("products", JSON.stringify(productsContainer));
-//     displayProduct();
-//     // clearForm();
+if (localStorage.getItem('products') == null) {
+    productContainer = [];
+}
+else {
+    productContainer = JSON.parse(localStorage.getItem('products'));
+    displayProduct(productContainer);
+}
 
 
-// }
+function addProduct() {
+    var product = {
+        name: productNameInput.value,
+        price: productPriceInput.value,
+        category: productCategoryInput.value,
+        desc: productDescInput.value,
+    }
+    productContainer.push(product)
+    localStorage.setItem('products', JSON.stringify(productContainer));
+    displayProduct(productContainer);
+    clearForm();
+}
 
+function displayProduct(produtsList) {
+    var cartoona = ``;
+    for (var i = 0; i < produtsList.length; i++) {
+        cartoona += `<tr>
+                <td>${i}</td>
+                <td>${produtsList[i].name}</td>
+                <td>${produtsList[i].price}</td>
+                <td>${produtsList[i].category}</td>
+                <td>${produtsList[i].desc}</td>
+                <td><button class="rounded-pill btn btn-danger">Update</button></td>
+                <td><button onclick ="deleteProduct (${i});" class=" px-3 rounded-pill btn btn-success">Delete</button></td>
+                </tr>`
+    }
+    document.getElementById('tboody').innerHTML = cartoona;
 
-// function displayProduct() {
-//     var cartoona = ``;
-//     for (var i = 0; i < productsContainer.length; i++) {
-
-//         cartoona += `<tr>
-//                 <td>${i}</td>
-//                 <td>${productsContainer[i].name}</td>
-//                 <td>${productsContainer[i].price}</td>
-//                 <td>${productsContainer[i].category}</td>
-//                 <td>${productsContainer[i].desc}</td>
-//                 <td><button onclick ="updateProduct (${i})"  class="btn btn-danger">update</button></td>
-//                 <td><button onclick = "deleteProduct (${i});" class="btn btn-info">delete</button></td>
-//                 </tr>`
-//     }
-//     document.getElementById('tboody').innerHTML = cartoona;
-// }
-
-
-// function deleteProduct(x) {
-//     productsContainer.splice(x, 1)
-//     displayProduct();
-// }
-
-// function updateProduct(i) {
-//     productsContainer[i].name;
-// }
-
+}
 
 // function clearForm() {
 //     productNameInput.value = '';
@@ -64,74 +50,18 @@
 //     productDescInput.value = '';
 // }
 
-var productNameInput = document.getElementById('productName');
-var productPriceInput = document.getElementById('productPrice');
-var productCategoryInput = document.getElementById('productCategory');
-var productDescInput = document.getElementById('productDesc');
-var productContainer;
-
-if(localStorage.getItem('products') == null)
-{
-    productContainer = [];
-}
-else
-{
-    productContainer = JSON.parse(localStorage.getItem('products'));
+function deleteProduct(x) {
+    productContainer.splice(x, 1);
+    localStorage.setItem('products', JSON.stringify(productContainer));
     displayProduct(productContainer);
-    
-
 }
 
-
-function addProduct ()
-{
-    var product = {
-        name:productNameInput.value,
-        price:productPriceInput.value,
-        category:productCategoryInput.value,
-        desc:productDescInput.value,
+function searchProduct(term) {
+    var searchProduct = [];
+    for (var i = 0; i < productContainer.length; i++) {
+        if (productContainer[i].name.toLowerCase().includes(term.toLowerCase()) == true) {
+            searchProduct.push(productContainer[i]);
+        }
     }
-    productContainer.push(product)
-    localStorage.setItem('products',JSON.stringify(productContainer));
-    var z = localStorage.length;
-    console.log(z)
-    console.log(productContainer)
-    displayProduct(productContainer);
-    clearForm ();
-}
-
-function displayProduct (produtsList)
-{
-    var cartoona = ``;
-    for (var i=0 ; i<produtsList.length ; i++)
-    {
-        cartoona += `<tr>
-                <td>${i}</td>
-                <td>${produtsList[i].name}</td>
-                <td>${produtsList[i].price}</td>
-                <td>${produtsList[i].category}</td>
-                <td>${produtsList[i].desc}</td>
-                <td><button class="btn btn-danger">Update</button></td>
-                <td><button onclick ="deleteProduct (${i});" class="btn btn-success">Delete</button></td>
-                </tr>`
-    }
-    document.getElementById('tboody').innerHTML=cartoona;
-    
-}
-
-function clearForm ()
-{
-    productNameInput,value = '';
-    productPriceInput.value = '';
-    productCategoryInput.value = '';
-    productDescInput.value = '';
-}
-
-function deleteProduct (x)
-{
-    productContainer.splice(x,1);
-    localStorage.setItem('products',JSON.stringify(productContainer));
-    displayProduct(productContainer);
-    console.log(productContainer)
-
+    displayProduct(searchProduct)
 }
